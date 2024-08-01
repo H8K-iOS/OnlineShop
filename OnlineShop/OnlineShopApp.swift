e581e7a1b5d46f8ee76198915f810d130f0e1317
H8K-iOS <alexalimov17@gmail.com>
2024-08-01 14:45:47 +0200

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Alexandr Alimov on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
